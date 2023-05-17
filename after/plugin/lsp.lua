f220bc3fbc4b36ebdfa6c179070d81b73dcb8a6e
Omar <o.raisi@hotmail.com>
2023-05-17 13:40:11 +0400

    removes keybindings in insert mode

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -47,7 +47,6 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set("n", "<leader>lac", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>lar", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<leader>lh", function() vim.lsp.buf.signature_help() end, opts)
 
 end)
 
