3679df3ed6754ab4598516f6722be984b12d9105
OmarAlraisi <o.r.raisi@gmail.com>
2024-06-25 00:41:37 +0400

    upgrades telescope

--- lua/omar/packer.lua
+++ lua/omar/packer.lua
@@ -16,7 +16,7 @@ return require('packer').startup(function(use)
 
     -- For file finding
 	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
